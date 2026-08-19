3913eb2b1312b527d737a90640dd83389978a18a
Csituka_D <694428809@qq.com>
2026-08-19 23:06:55 +0800

    add error page

---
+++ app/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="error-page">
+    {{ errorCode }}
+  </main>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+const errorCode = computed(() => props.error?.statusCode ?? 500)
+</script>
+
+<style scoped>
+.error-page {
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+}
+</style>
